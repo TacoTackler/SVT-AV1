6ec47c1850c5de92cd4bdf24e1c797b6445f93bc
edmond-zhu <edmond.zhu@cidana.com>
2019-08-05 15:38:17 +0800

    Add high bit-depth variance unit test

--- test/VarianceTest.cc
+++ test/VarianceTest.cc
@@ -341,19 +341,159 @@ INSTANTIATE_TEST_CASE_P(
         VarianceParam(8, 32, &eb_aom_variance8x32_c, &eb_aom_variance8x32_sse2),
         VarianceParam(16, 4, &eb_aom_variance16x4_c, &eb_aom_variance16x4_avx2),
         VarianceParam(16, 8, &eb_aom_variance16x8_c, &eb_aom_variance16x8_avx2),
-        VarianceParam(16, 16, &eb_aom_variance16x16_c, &eb_aom_variance16x16_avx2),
-        VarianceParam(16, 32, &eb_aom_variance16x32_c, &eb_aom_variance16x32_avx2),
-        VarianceParam(16, 64, &eb_aom_variance16x64_c, &eb_aom_variance16x64_avx2),
+        VarianceParam(16, 16, &eb_aom_variance16x16_c,
+                      &eb_aom_variance16x16_avx2),
+        VarianceParam(16, 32, &eb_aom_variance16x32_c,
+                      &eb_aom_variance16x32_avx2),
+        VarianceParam(16, 64, &eb_aom_variance16x64_c,
+                      &eb_aom_variance16x64_avx2),
         VarianceParam(32, 8, &eb_aom_variance32x8_c, &eb_aom_variance32x8_avx2),
-        VarianceParam(32, 16, &eb_aom_variance32x16_c, &eb_aom_variance32x16_avx2),
-        VarianceParam(32, 32, &eb_aom_variance32x32_c, &eb_aom_variance32x32_avx2),
-        VarianceParam(32, 64, &eb_aom_variance32x64_c, &eb_aom_variance32x64_avx2),
-        VarianceParam(64, 16, &eb_aom_variance64x16_c, &eb_aom_variance64x16_avx2),
-        VarianceParam(64, 32, &eb_aom_variance64x32_c, &eb_aom_variance64x32_avx2),
-        VarianceParam(64, 64, &eb_aom_variance64x64_c, &eb_aom_variance64x64_avx2),
-        VarianceParam(64, 128, &eb_aom_variance64x128_c, &eb_aom_variance64x128_avx2),
-        VarianceParam(128, 64, &eb_aom_variance128x64_c, &eb_aom_variance128x64_avx2),
+        VarianceParam(32, 16, &eb_aom_variance32x16_c,
+                      &eb_aom_variance32x16_avx2),
+        VarianceParam(32, 32, &eb_aom_variance32x32_c,
+                      &eb_aom_variance32x32_avx2),
+        VarianceParam(32, 64, &eb_aom_variance32x64_c,
+                      &eb_aom_variance32x64_avx2),
+        VarianceParam(64, 16, &eb_aom_variance64x16_c,
+                      &eb_aom_variance64x16_avx2),
+        VarianceParam(64, 32, &eb_aom_variance64x32_c,
+                      &eb_aom_variance64x32_avx2),
+        VarianceParam(64, 64, &eb_aom_variance64x64_c,
+                      &eb_aom_variance64x64_avx2),
+        VarianceParam(64, 128, &eb_aom_variance64x128_c,
+                      &eb_aom_variance64x128_avx2),
+        VarianceParam(128, 64, &eb_aom_variance128x64_c,
+                      &eb_aom_variance128x64_avx2),
         VarianceParam(128, 128, &eb_aom_variance128x128_c,
                       &eb_aom_variance128x128_avx2)));
 
+// High bit-depth variance test
+using HbdVarianceParam = std::tuple<uint32_t,  /**< width */
+                                    uint32_t,  /**< height */
+                                    uint32_t>; /**< bit-depth */
+
+class HbdVarianceTest : public ::testing::TestWithParam<HbdVarianceParam> {
+  public:
+    HbdVarianceTest()
+        : width_(TEST_GET_PARAM(0)),
+          height_(TEST_GET_PARAM(1)),
+          bd_(TEST_GET_PARAM(2)) {
+        src_data_ = reinterpret_cast<uint16_t *>(
+            eb_aom_memalign(32, 2 * MAX_BLOCK_SIZE));
+        ref_data_ = reinterpret_cast<uint16_t *>(
+            eb_aom_memalign(32, 2 * MAX_BLOCK_SIZE));
+    }
+
+    ~HbdVarianceTest() {
+        eb_aom_free(src_data_);
+        src_data_ = nullptr;
+        eb_aom_free(ref_data_);
+        ref_data_ = nullptr;
+    }
+
+    uint64_t cal_variance(uint64_t sse) {
+        int sum = 0;
+        for (uint32_t h = 0; h < height_; h++) {
+            for (uint32_t w = 0; w < width_; w++) {
+                const int diff =
+                    src_data_[h * width_ + w] - ref_data_[h * width_ + w];
+                sum += diff;
+            }
+        }
+        return ((uint64_t)(sum * sum) / (height_ * width_)) - sse;
+    }
+
+    void run_zero_test() {
+        for (int i = 0; i < (1 << bd_); ++i) {
+            // memset(src_data_, i, MAX_BLOCK_SIZE);
+            eb_aom_memset16(src_data_, i, MAX_BLOCK_SIZE);
+            for (int j = 0; j < (1 << bd_); ++j) {
+                // memset(ref_data_, j, MAX_BLOCK_SIZE);
+                eb_aom_memset16(ref_data_, j, MAX_BLOCK_SIZE);
+                uint64_t sse64;
+                highbd_variance64_avx2(reinterpret_cast<uint8_t *>(src_data_),
+                                       width_,
+                                       reinterpret_cast<uint8_t *>(ref_data_),
+                                       width_,
+                                       width_,
+                                       height_,
+                                       &sse64);
+                ASSERT_EQ(cal_variance(sse64), 0UL)
+                    << "Variance is mismatched, src values: " << i
+                    << " ref values: " << j;
+            }
+        }
+    }
+
+    void run_match_test() {
+        SVTRandom rnd(16, false);
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j < MAX_BLOCK_SIZE; j++) {
+                src_data_[j] = rnd.random() && ((1 << bd_) - 1);
+                ref_data_[j] = rnd.random() && ((1 << bd_) - 1);
+            }
+            uint64_t sse64_c, sse64_asm;
+            highbd_variance64_c(reinterpret_cast<uint8_t *>(src_data_),
+                                width_,
+                                reinterpret_cast<uint8_t *>(ref_data_),
+                                width_,
+                                width_,
+                                height_,
+                                &sse64_c);
+            highbd_variance64_avx2(reinterpret_cast<uint8_t *>(src_data_),
+                                   width_,
+                                   reinterpret_cast<uint8_t *>(ref_data_),
+                                   width_,
+                                   width_,
+                                   height_,
+                                   &sse64_asm);
+            ASSERT_EQ(sse64_c, sse64_asm) << "Error at test index: " << i;
+        }
+    }
+
+    void run_one_quarter_test() {
+        const int half = width_ * height_ / 2;
+        eb_aom_memset16(src_data_, (1 << bd_) - 1, MAX_BLOCK_SIZE);
+        eb_aom_memset16(ref_data_, (1 << bd_) - 1, half);
+        eb_aom_memset16(ref_data_ + half, 0, half);
+        uint64_t sse64_asm, expected;
+        highbd_variance64_avx2(reinterpret_cast<uint8_t *>(src_data_),
+                               width_,
+                               reinterpret_cast<uint8_t *>(ref_data_),
+                               width_,
+                               width_,
+                               height_,
+                               &sse64_asm);
+        expected = width_ * height_ * ((1 << bd_) - 1) * ((1 << bd_) - 1) / 4;
+        ASSERT_EQ(cal_variance(sse64_asm), expected);
+    }
+
+  private:
+    uint16_t *src_data_;
+    uint16_t *ref_data_;
+    uint32_t width_;
+    uint32_t height_;
+    uint32_t bd_;
+};
+
+TEST_P(HbdVarianceTest, ZeroTest) {
+    run_zero_test();
+};
+
+TEST_P(HbdVarianceTest, MatchTest) {
+    run_match_test();
+};
+
+TEST_P(HbdVarianceTest, OneQuarterTest) {
+    run_one_quarter_test();
+};
+
+static const uint32_t WIDTH_TABLE[] = {4, 8, 16, 32, 64, 128};
+static const uint32_t HEIGHT_TABLE[] = {4, 8, 16, 32, 64, 128};
+static const uint32_t BD_TABLE[] = {8, 10, 12, 16};
+
+INSTANTIATE_TEST_CASE_P(Variance, HbdVarianceTest,
+                        ::testing::Combine(::testing::ValuesIn(WIDTH_TABLE),
+                                           ::testing::ValuesIn(HEIGHT_TABLE),
+                                           ::testing::ValuesIn(BD_TABLE)));
 }  // namespace
